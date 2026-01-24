963d8b55386f01259bd3f33c650fdbc2fa6b561b
harshhhh-2-2 <hd2hd2hd2006@gmail.com>
2026-01-25 01:06:52 +0530

    database connectivity [MongoDB]

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from backend.config.db import client
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"status": "FastAPI is live"}
+@app.on_event("startup")
+async def startup_db():
+    await client.admin.command("ping")
+    print("MongoDB connected")
+
+@app.on_event("shutdown")
+async def shutdown_db():
+    client.close()
--- backend/config/db.py
+++ backend/config/db.py
@@ -1,1 +1,13 @@
+import os
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
 
+# This line finds the .env file and loads it into your system's memory
+load_dotenv()
+
+# Now you pull the values out using os.getenv
+MONGO_URL = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME")
+
+client = AsyncIOMotorClient(MONGO_URL)
+database = client[DB_NAME]
